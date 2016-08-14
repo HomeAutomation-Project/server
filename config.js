106e0e479ae1732e0ff5573067e535dc6b2a4982
Aman Vishnani <aman62@gmail.com>
2016-08-14 15:38:53 -0400

    Config file added

---
+++ config.js
@@ -0,0 +1,37 @@
+var config ={};
+
+
+module.exports = function(type){
+  if(type==='heroku'||type==='C9')
+    {
+      config.PORT = process.env.PORT;
+      config.IP =process.env.IP;
+      config.DB_URL = '';
+      config.DB_USERNAME = '';
+      config.DB_PASSWORD = '';
+      config.DB_HOST = '';
+      config.DB_PORT = '';
+    }
+  else if(type==='openshift')
+    {
+      config.PORT = process.env.PORT;
+      config.IP =process.env.HOST || process.env.HOSTNAME;
+      config.DB_URL = process.env.MONGOHQ_URL;
+      config.DB_USERNAME = process.env.OPENSHIFT_MONGODB_DB_USERNAME;
+      config.DB_PASSWORD = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
+      config.DB_HOST = process.env.OPENSHIFT_MONGODB_DB_HOST;
+      config.DB_PORT = process.env.OPENSHIFT_MONGODB_DB_PORT;
+      config.APP_NAME = process.env.OPENSHIFT_APP_NAME;
+    }
+  else if(type==='codeanywhere' || type==='nitrous')
+    {
+      config.PORT = process.env.PORT;
+      config.IP =process.env.IP;
+    }
+  else
+    {
+      config.PORT = process.env.PORT||8080;
+      config.IP =process.env.IP||'127.0.0.1';
+    }
+  return config;
+}
