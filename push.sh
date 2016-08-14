58183b81aa03ed7c4a59bc7ed296a95f38c89d28
Aman Vishnani <aman62@gmail.com>
2016-08-14 16:02:06 -0400

    Added push shell script

---
+++ push.sh
@@ -0,0 +1,7 @@
+echo "******************* GIT PUSH *************************\n"
+git status
+git add -A
+echo "******************* Enter Commit Msg *************************\n"
+read msg
+git commit -m "$msg"
+git push
