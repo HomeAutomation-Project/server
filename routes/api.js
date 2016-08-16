f3f1f81832056a1fb4b128f32611f5aaf8f63aad
Aman Vishnani <aman62@gmail.com>
2016-08-16 00:54:10 -0400

    added few more apis

--- routes/api.js
+++ routes/api.js
@@ -4,19 +4,24 @@ var User = require('../model/User.model.js');
 var Room = require('../model/Room.model.js');
 var Place = require('../model/Place.model.js');
 
+/******************** / ****************************/
 
-/******************** / ************************/
 module.exports =  myRouter.get('/',function(req,res){
+   res.send('\\user - to fetch all users and \\user\\username to fetch specific yser details');
+});
+
+
+/******************** /user ************************/
+module.exports =  myRouter.get('/user',function(req,res){
     User.find({}, function(err, users) {
-  if (err) throw err;
-
-  // object of all the users
-  res.send(users);
-  console.log(users);
+  if (err) throw err;  
+      res.send(users);
+      
+      console.log(users);
 });
 });
 
-module.exports =  myRouter.post('/',function(req,res){
+module.exports =  myRouter.post('/user',function(req,res){
   
       var newUser = User({
       username: req.body.username,
@@ -37,12 +42,55 @@ module.exports =  myRouter.post('/',function(req,res){
     
 });
 
-/********************* /house-:hid ************************/
-module.exports =  myRouter.get('/house-:hid',function(req,res){
-    res.send(req.params.hid);
-  });
+/********************* /user/username *********************/
 
-module.exports =  myRouter.get('/house-:id/room-:rid',function(req,res){
-    res.send("house Number: "+req.params.rid+" room Number: "+req.params.id);
-  });
+module.exports =  myRouter.get('/user/:username',function(req,res){
+    User.find({'username':req.params.username}, function(err, users) {
+  if (err) throw err;
+  
+  
+      var temp ={};
+      temp.userdetails=JSON.parse(JSON.stringify(users[0]));
+      Place.find({"belongsTo":temp.userdetails.username}, function(err, places) {
+      if (err) throw err;
+              temp.placedetails=JSON.parse(JSON.stringify(places));
+      });
+  // object of all the users
+  res.send(temp);
+  console.log(temp);
+});
+});
+
+module.exports =  myRouter.put('/user/:username',function(req,res){
+  
+  User.findOneAndUpdate({ 'username': req.body.username },
+                        { 
+                        username: req.body.username,
+                        email: req.body.email,
+                        name:{
+                          first: req.body.first,
+                          last: req.body.last
+                        },
+                        password: req.body.password
+                        },
+                        function(err, user) {
+                        if (err) throw err;
+                        console.log(user);
+  });    
+});
+
+
+/********************* /user/username/name/ ********************/
+
+module.exports =  myRouter.get('/user/:username/:name',function(req,res){
+    User.find({'username':req.params.username}, function(err, users) {
+  if (err) throw err;
+
+  // object of all the users
+  res.send(users);
+  console.log(users);
+});
+});
+
+
 
