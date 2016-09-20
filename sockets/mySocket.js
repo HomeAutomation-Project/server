118e5b7c7c78d6258149baea0b613bf3415aa53a
Aman Vishnani <aman62@gmail.com>
2016-09-20 17:01:03 +0000

    Few Changes in Socket

--- sockets/mySocket.js
+++ sockets/mySocket.js
@@ -1,18 +1,22 @@
 var Room = require("../model/Room.model.js")
-
-
+var mongoose =  require("mongoose");
 module.exports = function(io){
   io.on('connection', function (socket) {
   socket.emit('Request', { message: 'Authenticate' });
   console.log('Connected');
+  
   socket.on('Authenticate', function (data) {
     console.log(data);
-    Room.findById(data._id)
+    
+    Room.find({'_id':data})
     .populate('switches isOf')
-    .exec(function(err,data){if(err) throw err;
-      console.log(data);
-      socket.emit('Success',data);
-    });
+    .exec(function(err,room){
+      if(err) throw err;
+      console.log(room);
+      socket.emit('Success',{"data":room});
+    })
   });
+  
+  socket.on('Request',function(data){});
 });
 }
