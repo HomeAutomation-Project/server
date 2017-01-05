a95dc435942bb60e70450eab54d5eb1ef96a5061
Aman Vishnani <aman62@gmail.com>
2017-01-05 21:31:30 +0000

    bug fixes

--- config/config.js
+++ config/config.js
@@ -20,33 +20,20 @@ module.exports = function(){
     }
     else if(type==='openshiftv3')
     {
-            var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-      ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-      mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-      mongoURLLabel = "";
-  
-      if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-        var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-            mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-            mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-            mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-            mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
-            mongoUser = process.env[mongoServiceName + '_USER'];
+      var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
+      mongoUser = process.env[mongoServiceName + '_USER'];
+      config.DB_HOST =mongoHost;
+      config.DB_PORT = mongoPort;
+      config.DB_USERNAME =mongoUser;
+      config.DB_PASSWORD =mongoPassword;
+      config.PORT=  process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT|| 8080;
+      config.IP = process.env.OPENSHIFT_NODEJS_IP ||process.env.IP|| '0.0.0.0';
+      config.DB_URL ='mongodb://'+mongoUser+':'+mongoPassword+'@'+mongoHost+':'+mongoPort+'/'+mongoDatabase;
       
-        if (mongoHost && mongoPort && mongoDatabase) {
-          mongoURLLabel = mongoURL = 'mongodb://';
-          if (mongoUser && mongoPassword) {
-            mongoURL += mongoUser + ':' + mongoPassword + '@';
-          }
-        // Provide UI label that excludes user id and pw
-        mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-        mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-        config.DB_URL = mongoURL;
-        config.IP = ip;
-        config.PORT = port;
-      }
-      
-    }
     }
   else
     {
