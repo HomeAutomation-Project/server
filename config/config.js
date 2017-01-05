599d5d5897aed58e1c92536a1b849e0b44129deb
Aman Vishnani <aman62@gmail.com>
2017-01-05 18:51:02 +0000

    added support for Openshift V3

--- config/config.js
+++ config/config.js
@@ -18,6 +18,34 @@ module.exports = function(){
       config.APP_NAME = process.env.OPENSHIFT_APP_NAME || 'amanv';
       config.DB_URL = util.format("mongodb://%s:%s@%s:%d/%s",config.DB_USERNAME,config.DB_PASSWORD,config.DB_HOST,config.DB_PORT,config.APP_NAME);
     }
+    else if(type==='openshiftv3')
+    {
+            var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+      ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+      mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+      mongoURLLabel = "";
+  
+      if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+        var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+            mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+            mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+            mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+            mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
+            mongoUser = process.env[mongoServiceName + '_USER'];
+      
+        if (mongoHost && mongoPort && mongoDatabase) {
+          mongoURLLabel = mongoURL = 'mongodb://';
+          if (mongoUser && mongoPassword) {
+            mongoURL += mongoUser + ':' + mongoPassword + '@';
+          }
+        // Provide UI label that excludes user id and pw
+        mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+        mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+    
+      }
+      
+    }
+    }
   else
     {
         config.PORT = process.env.PORT||process.env.OPENSHIFT_NODEJS_PORT||8080;
